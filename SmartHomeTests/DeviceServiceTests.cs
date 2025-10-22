9d093bc23c8b7baf4435f82e779c71a657dd4cac
osmozisjones <arturoleksii@gmail.com>
2025-10-22 20:17:59 +0300

    Add project files.

---
+++ SmartHomeTests/DeviceServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Energy_Project.Services.Interfaces;
+using Moq;
+using Energy_Project.Models;
+using Energy_Project.Services;
+using Energy_Project.Services.Interfaces;
+using Moq;
+
+namespace SmartHomeTests
+{
+    public class DeviceServiceTests
+    {
+        private readonly Mock<IDeviceRepository> _deviceRepo = new();
+        private readonly Mock<IEnergyPlanRepository> _planRepo = new();
+        private readonly Mock<INotificationService> _notify = new();
+    }
+}
