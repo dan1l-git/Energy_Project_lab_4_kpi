d2f50dfe13f6978c658d205b49e1ab66f9212901
dan1l-git <danilgavriluk756@gmail.com>
2025-10-31 12:54:54 +0200

    EnergyMonitorServiceTests constructor implemented.

---
+++ SmartHomeTests/EnergyMonitorServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Energy_Project.Services.Interfaces;
+using Moq;
+using Energy_Project.Models;
+using Energy_Project.Services;
+using Energy_Project.Services.Interfaces;
+using Moq;
+namespace SmartHomeTests
+{
+    public class EnergyMonitorServiceTests
+    {
+        private readonly Mock<IDeviceRepository> _deviceRepo = new();
+        private readonly Mock<IEnergyPlanRepository> _planRepo = new();
+        private readonly Mock<INotificationService> _notify = new();
+        private readonly EnergyMonitorService _service;
+
+        public EnergyMonitorServiceTests()
+        {
+            _deviceRepo = new Mock<IDeviceRepository>();
+            _planRepo = new Mock<IEnergyPlanRepository>();
+            _notify = new Mock<INotificationService>();
+            
+            _service = new EnergyMonitorService(
+                _deviceRepo.Object,
+                _planRepo.Object,
+                _notify.Object
+            );
+        }
+    }
+}
